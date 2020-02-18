e79b8cadd5b43b8e79eccb17d70426df1e2a81a9
thinh1341993 <thinh1341993@gmail.com>
2020-02-18 07:09:30 +0700

    initial commit

--- Session03/Homework/sum.js
+++ Session03/Homework/sum.js
@@ -16,16 +16,35 @@
 // const arr = [3, 4, 6, -9, 10, -88, 2]
 // while(true){
 //     let nhap = Number(prompt('Enter the number'))
+//     let c = true
 // for(let i=0; i < arr.length; i++){
 //     if(arr[i]==nhap) {
-//         alert(nhap +' is found in my array at index'+ arr.indexOf(nhap))
-//         }
-//     // else {
-//     //     alert(nhap +' is not found in my array at index')
-//     //     break
-//     // }
+//         c = false
+//         break
 //     }
+//     }
+//     if(c){
+//     alert(nhap +' is not found in my array')}
+//     else {
+//         alert(nhap +' is found in my array at index'+ arr.indexOf(nhap))}
 // }
+
+// 8.You are a shepherd who owns a flock of 
+
+// let sizes = [5,7,300,90,24,50,75]
+// alert('Hello, my name is Phuong Nam and here is my sheep sizes:' +'\n'+sizes.join(' '))
+// alert('Now my biggest sheep has size '+ Math.max(...sizes)+", let's shave it")
+// sizes[sizes.indexOf(Math.max(...sizes))] = 8
+// alert('After shearing, here is my flock' +'\n'+sizes.join(' '))
+// for(j=1;j<4;j++){
+// for(i=0;i<sizes.length;i++){
+//     sizes[i] = sizes[i] + 50
+// }
+// alert('Month '+j+'\n'+'One month has, passed, my sheeps have grown, here are their sizes'+'\n'+sizes)
+// }
+// let total = sizes.reduce((ac,cu)=>ac+cu)
+// alert('My flock has size in total: '+total+'\n'+'I would get '+total+'*2$ ='+total*2)
+
 
 // 9.Turtle exercices
 // clear()
@@ -38,3 +57,14 @@
 //   rt(90)}
 //   a = a + 10}
   
+// 10.new array each surrounded with <>
+// let ten = prompt('Enter a sequeence of names')
+// let array = ten.split(',')
+// let new_array = array.map(x => '<'+x+'>')
+// alert(new_array)
+
+// 11.new array containing only the odd Numbers
+let number = prompt('Enter a sequeence number')
+let array = number.split(',')
+let new_array = array.filter(x=> x % 2 !== 0)
+alert(new_array)
